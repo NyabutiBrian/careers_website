8483419de0323b2ded7cea5e4ec71c5838dd4a1a
Momanyi Poet <momanyiturner@gmail.com>
2024-03-04 20:14:09 +0300

    added button name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar({activeLink}) {
             {/* Nav-item-left */}
             <div className="navbar-start text-secondary font-semibold">
                 <div className="dropdown">
-                    <button tabIndex="0" type='button' name='button' role="button" className="btn btn-ghost lg:hidden">
+                    <button tabIndex="0" type='button' name='button' aria-label="toggle navbar" role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
